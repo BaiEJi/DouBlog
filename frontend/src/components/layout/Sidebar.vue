6bc5173f7e9857813b3eb8d266ff5e65de67ca06
lizy <931469494@qq.com>
2026-04-07 23:06:15 +0800

    fix(layout): unify sidebar width to w-sidebar class

--- frontend/src/components/layout/Sidebar.vue
+++ frontend/src/components/layout/Sidebar.vue
@@ -3,7 +3,7 @@ import PostTree from '@/components/post/PostTree.vue'
 </script>
 
 <template>
-  <aside class="fixed left-0 top-14 w-70 h-[calc(100vh-56px)] bg-vscode-bg-secondary border-r border-vscode-border">
+  <div class="h-full overflow-auto">
     <PostTree />
-  </aside>
+  </div>
 </template>
